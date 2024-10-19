164442cb5b6646fea7b4b3fe94fc72ed76b1f008
lilicoronado71 <lilicoronado0207@gmail.com>
2024-10-19 16:50:20 -0500

    Se agrega menú hamburguesa y funcionalidad pagina indez

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,22 @@
+const nav = document.querySelector("#navegacion");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+//console.log("entró al java")
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+
+
+
+
 function catList(){
     //alert("Entro a la función catList")
     location.href ="./gaticos.html";
