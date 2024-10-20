be4f7fc3536f1892232b1a3f265a0d41b02bb089
lilicoronado71 <lilicoronado0207@gmail.com>
2024-10-19 20:45:59 -0500

    Se agrega formulario de login y registro en la pagina regAdopt

--- scripts/regAdopt.js
+++ scripts/regAdopt.js
@@ -1,8 +1,8 @@
+//Funcionalidad del menú hamburguesa
+//console.log("entró al javascript")
 const nav = document.querySelector("#navegacion");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
-
-//console.log("entró al java")
 
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
@@ -14,16 +14,69 @@ cerrar.addEventListener("click", () => {
 })
 
 
+//Funcionalidad de login y registro
+document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
+document.getElementById("btn__registrarse").addEventListener("click", register);
 
+window.addEventListener("resize", anchoPagina);
 
-//console.log("Hola mundo")
-let contenedor_login_register=document.getElementsByClassName(".contenedor__login-register");
-let formulario_login=document.getElementsByClassName(".formulario__login");
-let formulario_register=document.getElementsByClassName(".formulario_register");
-let caja_trasera_login=document.getElementsByClassName(".caja__trasera-login");
-let caja_trasera_register=document.getElementsByClassName(".caja__trasera-register");
+//Declaración de variables
+let contenedor_login_register=document.querySelector(".contenedor__login-register");
+let formulario_login=document.querySelector(".formulario__login");
+let formulario_register=document.querySelector(".formulario__register");
+let caja_trasera_login=document.querySelector(".caja__trasera-login");
+let caja_trasera_register=document.querySelector(".caja__trasera-register");
 
+function anchoPagina(){
+    if(window.innerWidth > 768){
+        caja_trasera_login.style.display = "block";
+        caja_trasera_register.style.display = "block";
+    }else{
+        caja_trasera_register.style.display = "block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.display = "none";
+        formulario_login.style.display = "block";
+        formulario_register.style.display = "none";
+        contenedor_login_register.style.left = "0px";
+    }
+}
 
-function register(){
+anchoPagina();
+
+//Funcion Iniciar Sesión
+function iniciarSesion(){
+    if(window.innerWidth > 768){
+        formulario_register.style.display = "none";
+        contenedor_login_register.style.left = "10px";
+        formulario_login.style.display = "block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.opacity = "0";
+    }else{
+        formulario_register.style.display = "none";
+        contenedor_login_register.style.left = "0px";
+        formulario_login.style.display = "block";
+        caja_trasera_register.style.display = "block";
+        caja_trasera_login.style.display = "none";
+    }
 
 }
+//Funcion de Registro
+function register(){
+    if(window.innerWidth > 768){
+        formulario_register.style.display = "block";
+        contenedor_login_register.style.left = "410px";
+        formulario_login.style.display = "none";
+        caja_trasera_register.style.opacity = "0";
+        caja_trasera_login.style.opacity = "1";
+    }else{
+        formulario_register.style.display = "block";
+        contenedor_login_register.style.left = "0px";
+        formulario_login.style.display = "none";
+        caja_trasera_register.style.display = "none";
+        caja_trasera_login.style.display = "block";
+        caja_trasera_login.style.opacity = "1";
+    }
+    
+
+}
+
