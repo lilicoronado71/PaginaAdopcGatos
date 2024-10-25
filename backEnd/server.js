b6c93a0e294c27370bfbd3e425be696b818bfbf3
lilicoronado71 <lilicoronado0207@gmail.com>
2024-10-24 21:45:19 -0500

    ejercicio clase mongo y coneción a la BD

---
+++ backEnd/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+//Inicialización de la aplicación Express
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Middlewares
+app.use(cors()); //Habilitar CORS para permitir las solicitudes desd el forntEnd
+app.use(express.json()); //Middleware para manejar JSON en lugar de body-parser
+app.use(express.urlencoded({ extended: true })); //Para manejar solicitudes de formulario
+
+//Conexion a la base de datos MongoDB
+mongoose.connect('mongodb://localhost:27017/AdopcGatico')
+    .then(() => console.log('Conectado correctamente a MongoDB'))
+    .catch(error => {
+        console.error('Error al conectar a MongoDB', error);
+        process.exit(1); //Salir si no se puede conectar a la base de datos
+    });
+
+//Definir el esquema y modelo de Mongoose para el registro de gaticos
+const registroGaticosSchema = new mongoose.Schema({
+    nombGato: { type: String, required: true },
+    raza: { type: String, required: true },
+    edad: { type: Number, min: 0, required: true },
+    madurez: { type: String, required: true },
+    colorPelaje: { type: Array, required: true },
+    sexo: { type: String, required: true },
+    esterilizado: { type: Boolean, required: true }
+    //faltan ítems
+}, { timestamps: true }); //Añadir timestamps para añadir fecha de creación (createdAt) y modificacipon (updatedAt)
+
+const regGaticos = mongoose.model('registroGatico', registroGaticosSchema);
+
+//Rutas
+//Ruta GET para obtener todos los ítems de la colección
+app.get('/api/obtenerRegistroGatico', async (req, res) => {
+    try {
+        const razas = await regGaticos.find(); //buscar todas las raazas de gatos existentes
+        res.status(200).json(razas);  //Responder con el registro de razas
+    } catch (error) {
+        res.status(500).json({ error: 'Error al obtener el registro de gatos' });
+    }
+});
+
+
+
+//Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
--- scripts/gaticos.js
+++ scripts/gaticos.js
@@ -13,86 +13,108 @@ cerrar.addEventListener("click", () => {
 
 
 
-function searchCat(){
-   alert("Entró a la función");
-}
+window.addEventListener("load", function(event) {
+    //alert("Entró a la función");
 
+    //Lista desplegable de razas de gatos
+    let CantGatos = [
+        {
+            "raza": "Africano Doméstico",
+            "cantidad": 2
+        },
+        {
+            "raza": "Angora",
+            "cantidad": 5
+        },
+        {
+            "raza": "Azul Ruso",
+            "cantidad": 1
+        },
+        {
+            "raza": "Balinés",
+            "cantidad": 2
+        }, {
+            "raza": "Bombay",
+            "cantidad": 10
+        },
+        {
+            "raza": "Bosque de Siberia",
+            "cantidad": 1
+        },
+        {
+            "raza": "Burmilla",
+            "cantidad": 2
+        },
+        {
+            "raza": "Calico",
+            "cantidad": 3
+        },
+        {
+            "raza": "Carey",
+            "cantidad": 3
+        },
+        {
+            "raza": "Criollo",
+            "cantidad": 7
+        },
+        {
+            "raza": "German Rex",
+            "cantidad": 1
+        },
+        {
+            "raza": "Leopardo Doméstico",
+            "cantidad": 1
+        },
+        {
+            "raza": "Persa de Pelo Negro",
+            "cantidad": 4
+        },
+        {
+            "raza": "Tabby",
+            "cantidad": 7
+        }
+    ];
 
-let CantGatos = [
-    {
-        "raza":"Africano Doméstico",
-        "cantidad":2
-    },
-    {
-        "raza":"Angora",
-        "cantidad":5
-    },
-    {
-        "raza":"Azul Ruso",
-        "cantidad":1
-    },
-    {
-        "raza":"Balinés",
-        "cantidad":2
-    }, {
-        "raza":"Bombay",
-        "cantidad":10
-    },
-    {
-        "raza":"Bosque de Siberia",
-        "cantidad":1
-    },
-    {
-        "raza":"Burmilla",
-        "cantidad":2
-    },
-    {
-        "raza":"Calico",
-        "cantidad":3
-    },
-    {
-        "raza":"Carey",
-        "cantidad":3
-    },
-    {
-        "raza":"Criollo",
-        "cantidad":7
-    },
-    {
-        "raza":"German Rex",
-        "cantidad":1
-    },
-    {
-        "raza":"Leopardo Doméstico",
-        "cantidad":1
-    },
-    {
-        "raza":"Persa de Pelo Negro",
-        "cantidad":4
-    },
-    {
-        "raza":"Tabbyo",
-        "cantidad":7
+    let select = document.getElementById("listaRazasGatos");
+    
+    for (i = 0; i < CantGatos.length; i++) {
+        //console.log("i: "+i);
+        let option = document.createElement('OPTION');
+        option.innerHTML = CantGatos[i].raza;
+        option.value = CantGatos[i].raza;
+        select.options.add(option);
+        //console.log(CantGatos[i].raza);
     }
-];
+});
 
-//console.log(CantGatos[1].raza);
-//console.log(CantGatos[1].cantidad);
-
-// CantGatos.forEach(function(razaCantidad) {
-//     console.log(razaCantidad);
-// });
-
-// CantGatos.forEach(function(recorrido) {
-//     console.log(recorrido.raza);
-// });
-
-//Ejemplo For
-
-for(i=0; i<CantGatos.length; i++){
-    console.log("i: "+i);
-    console.log(CantGatos[i]);
-    if(CantGatos[i].cantidad == 3){
-        console.log("Estas razas tienen 3");
-    }
+function ShowSelected(){
+    /* Para obtener el valor seleccionado */
+    var cod = document.getElementById("listaRazasGatos").value;
+    console.log("value:  " + cod);
+    
+    // /* Para obtener el texto */
+    // var combo = document.getElementById("listaRazasGatos");
+    // var selected = combo.options[combo.selectedIndex].text;
+    // alert("texto" + selected);
 }
+ function BuscarGatos(){
+    alert("entro a la funciión BuscarGatos");
+    
+ }
+
+
+
+
+
+
+
+    //console.log(CantGatos[1].raza);
+    //console.log(CantGatos[1].cantidad);
+
+    // CantGatos.forEach(function(razaCantidad) {
+    //     console.log(razaCantidad);
+    // });
+
+    // CantGatos.forEach(function(recorrido) {
+    //     console.log(recorrido.raza);
+    // });
